8e9abbf7892cacfcd6d576daff075a0de5f498cc
suhaildhada <suhaildhada123@gmail.com>
2024-02-09 19:32:15 +0530

    changed bg img to be blurred

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,36 @@
-import TECHEON_LOGO from "../assets/techeon-logo.png";
 import CBIT_LOGO from "../assets/cbit-logo.png";
+import TECHEON_POSTER from "../assets/techeon-poster-cropped-resized.jpg";
 import { BiRightArrowAlt } from "react-icons/bi";
 import { Link } from "react-router-dom";
 export function Home() {
   return (
-    <div className="h-full w-full flex">
-      <div className="flex flex-col justify-center items-center w-full p-10">
-        <img src={CBIT_LOGO} alt="CBIT Logo" className="my-1" />
-        <img src={TECHEON_LOGO} alt="Techeon Logo" className="my-2" />
-        <h1 className="text-3xl md:text-4xl font-bold pt-10">Techeon 2024</h1>
-        <p className="text-2xl font-medium">
-          Innovate. Integrate. Inspire: The Tech Revolution Unleashed.
-        </p>
-        <p className="text-3xl mt-5 font-bold">
-          Join us on 26th & 27th of February - 2024
-        </p>
+    <div className="h-full w-full flex bg-img">
+      <div className="flex flex-col justify-center items-center w-full p-10 md:p-24">
+        <div>
+          <img src={CBIT_LOGO} alt="CBIT Logo" className="w-[500px]" />
+        </div>
+        <div>
+          <img
+            src={TECHEON_POSTER}
+            alt="Techeon Logo"
+            className="my-2 rounded-lg border-2 border-white object-cover w-[800px]"
+          />
+        </div>
+        <div className="flex justify-center items-center flex-col bg-black/60 rounded-lg pb-3 px-3 border border-white">
+          <h1 className="text-xl md:text-3xl md:text-4xl font-bold p-2 text-white ">
+            Techeon 2024
+          </h1>
+          <p className="text-md md:text-2xl font-medium text-white">
+            Innovate. Integrate. Inspire: The Tech Revolution Unleashed.
+          </p>
+          <p className="text-xl md:text-3xl mt-5 font-bold text-white">
+            Join us on 26th & 27th of February - 2024
+          </p>
+        </div>
 
         <Link
           to="/events"
-          className="p-5  text-2xl sm:leading-none bg-[#020F1F] text-white rounded-lg mt-3 flex items-center justify-center"
+          className="p-3 md:p-5 text-lg md:text-2xl sm:leading-none bg-[#020F1F] text-white rounded-lg mt-3 flex items-center justify-center"
         >
           See Events
           <span className="m-0 p-0 inline-block ">
