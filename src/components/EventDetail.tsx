6c901b60659f48dfd236bea1f2f2ddc7e4a67f10
suhaildhada <suhaildhada123@gmail.com>
2024-02-14 00:24:14 +0530

    fixed register link not showing in light mode

--- src/components/EventDetail.tsx
+++ src/components/EventDetail.tsx
@@ -90,7 +90,7 @@ export function EventDetail() {
             href={googleFormLink}
             target="_blank"
             rel="noreferrer"
-            className="flex items-center justify-center gap-1  text-white max-w-32 p-1 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-2"
+            className="flex items-center justify-center gap-1 max-w-32 p-1 text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-2"
           >
             <MdAppRegistration size={20} />
             Register
